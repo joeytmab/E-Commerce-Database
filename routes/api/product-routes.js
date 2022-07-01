84b8f915141ada038eb96f87df8ab21844980017
Joey Binas <joseph.t.binas@gmail.com>
2022-06-30 18:46:40 -0700

    adjusted async

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }],
+    });
+    
+    res.status(200).json(prodData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }],
+    });
+
+    if (prodData) {
+      res.status(200).json(prodData);
+     
+    } else if (!prodData)
+      res.status(404).json({ message: "No product found with input ID." });
+      return;
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -89,8 +114,27 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const prodData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    
+    if (prodData) {
+      res.status(200).json(prodData);
+      return;
+
+    } else if (!prodData) {
+      res.status(404).json({ message: "No product found with input ID." });
+      return;
+    }
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
